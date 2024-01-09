79bffc01ede77b5f74d2d2ea27a446fdc4ce769b
ViththiL <viththisha68@gmail.com>
2024-01-09 10:14:56 +0530

    svelte modification

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const serverPath = process.pkg ? path.join(process.cwd(), '../') : process.cwd();
-
-app.use(express.static(path.join(serverPath, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(serverPath, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
