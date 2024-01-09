3170b4f163bd36fa7b3db266e6417667fa9617f4
ViththiL <viththisha68@gmail.com>
2024-01-09 10:53:44 +0530

    svelte modification

--- server.cjs
+++ server.cjs
@@ -4,15 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Handle all routes by serving the 'index.html' file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
